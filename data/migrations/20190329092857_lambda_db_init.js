7feb4c6370ab47ccb32c66817219d95cfe086a24
Clark <rclark010@gmail.com>
2019-03-29 09:47:09 -0600

    Fixed mistake in migration. Needed to write rollback function.

--- data/migrations/20190329092857_lambda_db_init.js
+++ data/migrations/20190329092857_lambda_db_init.js
@@ -31,4 +31,6 @@ exports.up = function(knex, Promise) {
     });
 };
 
-exports.down = function(knex, Promise) {};
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('actions').dropTableIfExists('projects');
+};
